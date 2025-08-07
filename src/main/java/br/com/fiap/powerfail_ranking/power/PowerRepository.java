be08b8e89fa415ce63eddcfe254536aa64a02788
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-07 10:42:35 -0300

    adicionando a interfce PowerRepository no package Power

---
+++ src/main/java/br/com/fiap/powerfail_ranking/power/PowerRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.powerfail_ranking.power;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PowerRepository extends JpaRepository<Power, Long>{
+
+
+}
