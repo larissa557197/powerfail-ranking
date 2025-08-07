22dc95a5353f0a76be5d6b06aab2fde6d0c3fc57
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-07 10:46:09 -0300

    adicionando a classe PowerService com o metodo 'inicial' pra retornar os poderes na ordem de inutilidade de 0 a 5

---
+++ src/main/java/br/com/fiap/powerfail_ranking/power/PowerService.java
@@ -0,0 +1,22 @@
+package br.com.fiap.powerfail_ranking.power;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PowerService {
+
+    private final PowerRepository powerRepository;
+
+    // construtor
+    public PowerService(PowerRepository powerRepository) {
+        this.powerRepository = powerRepository;
+    }
+
+    // metodo que retorna a orgem de inutilidade
+    public List<Power> getAllPowers(){
+        return powerRepository.findAll();
+    }
+    
+}
